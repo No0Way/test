30890b8a296aa1117f4423bdf3a1c5e47d0ff8b3
No0Way <enotywkaaaa@gmail.com>
2023-03-26 17:39:43 +0300

    edit 26.03

---
+++ Vscode/Lesson6/js/script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+// $('.block-lorem-cust').slick();
+// })
+// let burger-menu = document.querySelector('.menu-btn');
+// let nav-list = document.querySelector('.menu');
+// burger-menu.addEventListener('click', function(){
+    
+// 	nav-list.classList.toggle('active');
+// })
+
+// let burger-menu = document.querySelector('.menu-btn');
+// let nav-list = document.querySelector('.menu');
+// burger-menu.addEventListener('click', function(){
+// 	burger-menu.classList.toggle('active');
+// 	nav-list.classList.toggle('active');
+// })
