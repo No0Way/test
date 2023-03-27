d5388d2f4fe62ac62f00df492792c78ac833d1ce
No0Way <enotywkaaaa@gmail.com>
2023-03-27 17:54:29 +0300

    edit 27.03

--- Vscode/Lesson6/js/script.js
+++ Vscode/Lesson6/js/script.js
@@ -1,16 +1,34 @@
-// $(document).ready(function() {
-// $('.block-lorem-cust').slick();
-// })
-// let burger-menu = document.querySelector('.menu-btn');
-// let nav-list = document.querySelector('.menu');
-// burger-menu.addEventListener('click', function(){
-    
-// 	nav-list.classList.toggle('active');
-// })
+// new Swiper('.lorem-cont', {
+//     breakpoints: {
+//         450: {
+//             slidesPerView: 1,
+//         }
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     },
 
-// let burger-menu = document.querySelector('.menu-btn');
-// let nav-list = document.querySelector('.menu');
-// burger-menu.addEventListener('click', function(){
-// 	burger-menu.classList.toggle('active');
-// 	nav-list.classList.toggle('active');
-// })
+// });
+$('.lorem-content-main').slick({
+    dots: true,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    touchmove: true, 
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+    ]
+
+    
+  });
+
+  $('.burger-menu').click(function(event) {
+    $('.burger-menu,.nav-list').toggleClass('active');
+  });
